f3095b9213c4542c7ba85fe590ed717811c16ab1
Anggriawan Ridatama <anggriawanridatama@Anggriawans-Laptop.local>
2024-02-16 11:37:32 +0700

    Editing Navbar.tsx :hammer:

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="/logo.svg" width={115} height={43} alt="Flexibble" />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-7"></ul>
+      </div>
+      ;
+    </nav>
+  );
 };
 
 export default Navbar;
