35f06550b7d4d4b517c2a38d60e3c44a2b1da8a0
Anggriawan Ridatama <anggriawanridatama@Anggriawans-Laptop.local>
2024-02-16 11:26:54 +0700

    Adding Navbar and Footer :sparkles:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
 export const metadata = {
   title: "Flexibble",
   description: "Showcase and discover remarkable developer projects",
@@ -10,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
