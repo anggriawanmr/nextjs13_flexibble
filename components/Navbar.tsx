d64e213717ea2e0b4df1141cb180db6a2fdde96d
anggriawanmr <anggriawanmr@gmail.com>
2024-02-18 18:41:06 +0700

    Editing the Footer component :hammer:

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import AuthProviders from "./AuthProviders";
+import { NavLinks } from "@/constants";
 
 const Navbar = () => {
   const session = {};
